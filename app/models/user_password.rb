02f9cec6c09f5f4c5ede4b7fe689cbceb3306210
Erick <argolloerickdev@gmail.com>
2023-06-08 14:33:40 -0300

    feat: add roles validation to user_password model

--- app/models/user_password.rb
+++ app/models/user_password.rb
@@ -1,4 +1,8 @@
 class UserPassword < ApplicationRecord
+  ROLES = %w[owner editor viewer].freeze
+
   belongs_to :user
   belongs_to :password
+
+  validates :role, presence: true, inclusion: { in: ROLES }
 end
