04b43a01623664ec642d6c678baa0c32ac842049
Erick <argolloerickdev@gmail.com>
2023-06-08 12:26:46 -0300

    feat: add dependent destroy

--- app/models/password.rb
+++ app/models/password.rb
@@ -1,8 +1,7 @@
 class Password < ApplicationRecord
-  has_many :user_passwords
+  has_many :user_passwords, dependent: :destroy
   has_many :users, through: :user_passwords
 
-  encrypts :username, deterministic: true
   encrypts :password
 
   validates :username, presence: true
