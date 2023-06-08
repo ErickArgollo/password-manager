e10cba8408825e3635680e201834a4cc4236b16f
Erick <argolloerickdev@gmail.com>
2023-06-08 11:47:12 -0300

    refactor: password model

--- app/models/password.rb
+++ app/models/password.rb
@@ -2,10 +2,10 @@ class Password < ApplicationRecord
   has_many :user_passwords
   has_many :users, through: :user_passwords
 
-  encrypts :user_name, deterministic: true
+  encrypts :username, deterministic: true
   encrypts :password
 
-  validates :user_name, presence: true
+  validates :username, presence: true
   validates :password, presence: true
   validates :url, presence: true
 end
