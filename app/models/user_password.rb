21ab41d03c247b852bfb4fbbad185491bdba0c41
Erick <argolloerickdev@gmail.com>
2023-06-08 15:09:47 -0300

    feat: add roles helper methods

--- app/models/user_password.rb
+++ app/models/user_password.rb
@@ -5,4 +5,16 @@ class UserPassword < ApplicationRecord
   belongs_to :password
 
   validates :role, presence: true, inclusion: { in: ROLES }
+
+  def owner?
+    role == 'owner'
+  end
+
+  def editor?
+    role == 'editor'
+  end
+
+  def viewer?
+    role == 'viewer'
+  end
 end
