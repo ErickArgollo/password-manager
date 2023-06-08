7705e34d56c08bfd9dec17ac861fecb5422de278
Erick <argolloerickdev@gmail.com>
2023-06-08 13:57:30 -0300

    feat: setup shares controller with #new and #set_password

---
+++ app/controllers/shares_controller.rb
@@ -0,0 +1,20 @@
+class SharesController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_password
+
+  def new 
+    @users = User.all.excluding(current_user)
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+
+  private 
+
+  def set_password
+    @password = current_user.passwords.find(params[:password_id])
+  end
+end
