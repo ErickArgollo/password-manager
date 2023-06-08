771c53f952fbf8b0c8007131797c59907e7a155b
Erick <argolloerickdev@gmail.com>
2023-06-08 11:24:03 -0300

    chore: setup #new

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -2,5 +2,11 @@ class PasswordsController < ApplicationController
   before_action :authenticate_user!
 
   def index
+    @passwords = current_user.passwords
   end
+
+  def new
+    @password = Password.new
+  end
+  
 end
