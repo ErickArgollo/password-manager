4ef6a815469e9554a01a6fb2cc9d7ed5296cee81
Erick <argolloerickdev@gmail.com>
2023-06-08 11:46:56 -0300

    refactor: #create and add #set_password

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -1,5 +1,6 @@
 class PasswordsController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_password, only: [:show, :update, :destroy]
 
   def index
     @passwords = current_user.passwords
@@ -9,9 +10,12 @@ class PasswordsController < ApplicationController
     @password = Password.new
   end
 
+  def show 
+  end
+
   def create
-    @password = current_user.passwords.build(password_params)
-    if @password.save
+    @password = current_user.passwords.create(password_params)
+    if @password.persisted?
       redirect_to @password
     else
       render :new, status: :unprocessable_entity
@@ -21,6 +25,10 @@ class PasswordsController < ApplicationController
   private
 
   def password_params
-    params.require(:password).permit(:url, :user_name, :password)
+    params.require(:password).permit(:url, :username, :password)
+  end
+
+  def set_password
+    @password = current_user.passwords.find(params[:id])
   end
 end
